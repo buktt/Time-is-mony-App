537cdfce86f0f1979eef663a0e24a903c79782de
alon-apono <alon.m@apono.io>
2025-12-14 19:19:39 +0200

    Change base path to root

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Time-is-mony-App/',
+  base: '/',
   plugins: [
     react(),
     VitePWA({
